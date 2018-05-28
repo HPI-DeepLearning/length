ecc2827a089da1a5c33d2604b7135ac8d6063953
Joseph Bethge <joseph@visitare.de>
2018-05-28 13:07:50 +0200

    add solution for adam

--- length/optimizers/adam.py
+++ length/optimizers/adam.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from length.optimizer import Optimizer
@@ -18,9 +20,45 @@ class Adam(Optimizer):
         self.beta2 = beta2
         self.eps = eps
 
-        # TODO: add more initialization code
+        # map from layer id to a list of numpy arrays
+        self.m_values = {}
+        self.v_values = {}
+        # map from layer id to int (time step)
+        self.t_values = {}
+
+        self.current_id = -1
+        self._initialized = set()
 
     def run_update_rule(self, gradients, layer):
-        # TODO: implement Adam update rule as specified in https://arxiv.org/abs/1412.6980
-        param_deltas = [np.zeros_like(g) for g in gradients]
+        self.current_id = id(layer)
+
+        if not self.initialized:
+            self.initialize(gradients)
+
+        self.t_values[self.current_id] += 1
+        t = self.t_values[self.current_id]
+
+        param_deltas = []
+
+        for i, gradient in enumerate(gradients):
+            m = self.m_values[self.current_id][i]
+            v = self.v_values[self.current_id][i]
+
+            m += (1 - self.beta1) * (gradient - m)
+            v += (1 - self.beta2) * (gradient * gradient - v)
+
+            m_fix = m / (1 - self.beta1 ** t)
+            v_fix = v / (1 - self.beta2 ** t)
+
+            param_deltas.append(self.learning_rate * m_fix / (np.sqrt(v_fix) + self.eps))
         return param_deltas
+
+    def initialize(self, gradients):
+        self.m_values[self.current_id] = [np.zeros_like(gradient) for gradient in gradients]
+        self.v_values[self.current_id] = [np.zeros_like(gradient) for gradient in gradients]
+        self.t_values[self.current_id] = 0
+        self._initialized.add(self.current_id)
+
+    @property
+    def initialized(self):
+        return self.current_id in self._initialized
